fcd7bfd57a748926799c5360beb55deff4981054
Хизир Абдеев <abdeevhizir95@gmail.com>
2024-04-27 02:23:37 +0300

    Update debounce.tsx

--- src/components/debounce/debounce.tsx
+++ src/components/debounce/debounce.tsx
@@ -4,6 +4,10 @@ export const useDebounce = (value: string, delay: number): string => {
 	const [debouncedValue, setDebouncedValue] = useState(value);
 
 	useEffect(() => {
+		if (handler) {
+			clearTimeout(handler)
+		}
+		
 		const handler = setTimeout(() => {
 			setDebouncedValue(value);
 		}, delay);
